b236ed48487d6d844ac051dc5e59018b1765022a
GEETHA <128409213+geetha2543@users.noreply.github.com>
2025-04-05 15:32:21 +0530

    Add files via upload

---
+++ day2_assign.py
@@ -0,0 +1,33 @@
+
+"""
+Question-3:
+Given a directory, find out the file Name 
+having max size recursively 
+"""
+import os
+import glob
+path=r"C:\Users\Geetha\Downloads\handson\handson\DAY1"
+
+def max_file(directory,filename,maxsize):
+    p=directory+r"\*"
+    items=glob.glob(p)
+    for item in items:
+        if(os.path.isdir(item)):
+            maxsize,filename=max_file(item,filename,maxsize)
+        else:
+            if(os.path.isfile(item)):
+                size=os.path.getsize(item)
+                if(size>maxsize):
+                    maxsize=size;
+                    filename=os.path.basename(item)
+    return [maxsize,filename] 
+            
+
+
+
+print(max_file(path,"",0))
+    
+    
+    
+    
+    
