28d7d48e03284eb6be7966ea496e1225b015e5c4
GEETHA <128409213+geetha2543@users.noreply.github.com>
2025-04-05 13:06:16 +0530

    day1_assign.py

--- day1_assign.py
+++ day1_assign.py
@@ -6,20 +6,28 @@ d2 = {'ok': 2, 'new':3 }
 
 union=d1.copy()
 for keys in d2:
-    if keys not in union:
-        union[keys]=d2[keys]        
+    union[keys]=d2[keys]        
 print(union)
-#or
+                #or
 #print(d1|d2)
+
+
+#--------------------------------------------------------------------------------------
+
 
 #intersection
 
 intersection={}
 for key1 in d1:
     if key1 in d2:
-        intersection[key1]=d1[key1]
-        
+        intersection[key1]=d1[key1]  
 print(intersection)
+                #or
+intersection={keys:values for keys,values in d1.items() if keys in d2}
+#print(intersection)
+
+
+#--------------------------------------------------------------------------------------
 
 
 #d1-d2
@@ -28,8 +36,14 @@ only_d1={}
 for keys in d1:
     if keys not in d2:
         only_d1[keys]=d1[keys]
-
 print(only_d1)
+                #or
+only_d1={keys:values for keys,values in d1.items() if keys not in d2}
+print(only_d1)
+
+
+#--------------------------------------------------------------------------------------
+
 
 #merge
 
