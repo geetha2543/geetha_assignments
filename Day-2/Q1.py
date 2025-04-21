d20ff76451781bd886db8c4283b1395be1123725
GEETHA <128409213+geetha2543@users.noreply.github.com>
2025-04-21 17:07:19 +0530

    Q1.py

--- Day-2/Q1.py
+++ Day-2/Q1.py
@@ -5,19 +5,19 @@ import glob
 #Given a directory, find out the file Name 
 #having max size recursively 
 
-directory=r"C:\Users\Geetha\handson"
+directory = r"C:\Users\Geetha\handson"
 
 def max_file(directory,filename,maxsize):
-    p=directory+r"\*"
-    items=glob.glob(p)
+    p = directory+r"\*"
+    items = glob.glob(p)
     for item in items:
         if(os.path.isdir(item)):
-            maxsize,filename=max_file(item,filename,maxsize)
+            maxsize,filename = max_file(item,filename,maxsize)
         else:
-            size=os.path.getsize(item)
+            size = os.path.getsize(item)
             if(size>maxsize):
-                maxsize=size;
-                filename=os.path.basename(item)
+                maxsize = size;
+                filename = os.path.basename(item)
     return [maxsize,filename] 
 
 print(max_file(directory,"no file is present in the directory",0)[1])
